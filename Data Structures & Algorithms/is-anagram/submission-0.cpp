72537ef427bcb046d467eaf105a55fe12fc5473e
roopeshbrlearning-blip <roopeshbrlearning@gmail.com>
2026-05-18 16:50:23 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        std::unordered_map<char,int> map_s;
+        std::unordered_map<char,int> map_t;
+
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        for(auto c: s){
+            if(!map_s.count(c)){
+                map_s[c] = 1;
+            }
+            else{
+                map_s[c] += 1;
+            }
+        }
+
+        for(auto c: t){
+            if(!map_t.count(c)){
+                map_t[c] = 1;
+            }
+            else{
+                map_t[c] += 1;
+            }
+        }
+
+        if(map_s == map_t){
+            return true;
+        }
+
+        return false;
+        
+    }
+};
